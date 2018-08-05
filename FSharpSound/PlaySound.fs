a48b9797d0510c8981fc69bfdc9975d1949a0400
aosoft <tan-y@aosoft.jp>
2018-08-05 23:34:11 +0900

    Add PlaySound

---
+++ FSharpSound/PlaySound.fs
@@ -0,0 +1,46 @@
+﻿module FSharpSound.SoundIOOutStreamUtil
+
+#nowarn "9"
+
+open SoundIOSharp
+open FSharp.NativeInterop
+open System.Threading
+
+let playsound sampleRate (stream: System.Collections.Generic.IEnumerable<float32>) =
+    use api = new SoundIO()
+    api.Connect()
+    api.FlushEvents()
+    let device = api.GetOutputDevice(api.DefaultOutputDeviceIndex)
+    try
+        use outstream = device.CreateOutStream()
+        use waiter = new ManualResetEventSlim()
+        use en = stream.GetEnumerator()
+
+        outstream.WriteCallback <- fun frameCountMin frameCountMax ->
+            let mutable frameCount = 0
+            frameCount <- frameCountMax
+            let layout2 = outstream.Layout
+            let results = outstream.BeginWrite(ref frameCount)
+            try
+                let rec iterate () =
+                    if en.MoveNext() then
+                        let value = en.Current
+                        for c in 0..layout2.ChannelCount - 1 do
+                            let mutable area = results.GetArea(c)
+                            NativePtr.set (area.Pointer |> NativePtr.ofNativeInt<float32>) 0 value
+                            area.Pointer <- NativeInterop.NativePtr.add (area.Pointer |> NativePtr.ofNativeInt<byte>) area.Step |> NativePtr.toNativeInt
+                        iterate()
+                iterate()
+            finally
+                outstream.EndWrite()
+                waiter.Set()
+        outstream.Format <- SoundIODevice.Float32NE
+        outstream.SampleRate <- sampleRate
+        outstream.SoftwareLatency <- 0.0
+
+        outstream.Open()
+        outstream.Start()
+
+        waiter.Wait()
+    finally
+        device.RemoveReference()
--- FSharpSound/Program.fs
+++ FSharpSound/Program.fs
@@ -1,7 +1,11 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open System
+
+let sampleRate = 48000
+let pitch = 440.0
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    [0..sampleRate * 2]
+    |> List.map(fun x -> (float32)(Math.Sin(2.0 * Math.PI * pitch * (float)x / (float)sampleRate)))
+    |> FSharpSound.SoundIOOutStreamUtil.playsound sampleRate
     0 // return an integer exit code
