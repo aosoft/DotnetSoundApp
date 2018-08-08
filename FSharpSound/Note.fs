85059d7537a36c3d386b354f0abcd9aaae5d57d1
aosoft <tan-y@aosoft.jp>
2018-08-09 01:03:26 +0900

    Add C major sample
    * Add Note class
    * Add Square wave generator

---
+++ FSharpSound/Note.fs
@@ -0,0 +1,38 @@
+﻿
+namespace FSharpSound
+
+open System
+
+
+type NoteName =
+    | c = 0
+    | csh = 1
+    | d = 2
+    | dsh = 3
+    | e = 4
+    | f = 5
+    | fsh = 6
+    | g = 7
+    | gsh = 8
+    | a = 9
+    | ash = 10
+    | b = 11
+
+type Note =
+    struct
+        static member basePitch = 440.0
+ 
+        val noteName: NoteName
+        val octave: int
+        new(noteName: NoteName, octave: int) = { noteName = noteName; octave = octave }
+
+        member this.noteIndex  = this.octave * 12 + (int)this.noteName
+        member this.pitch =
+            Note.basePitch * Math.Pow(2.0, float (this.noteIndex - Note(NoteName.ash, 4).noteIndex) / 12.0)
+        member this.perCycle (sec: float) =
+            let x = sec * this.pitch
+            x - floor(x)
+
+    end
+
+
--- FSharpSound/Program.fs
+++ FSharpSound/Program.fs
@@ -1,11 +1,24 @@
 ﻿open System
+open FSharpSound
 
 let sampleRate = 48000
-let pitch = 440.0
+
+let samplesToSec sample = (float)sample / (float)sampleRate
+
+let sinWave t = sin(2.0 * Math.PI * t)
+let squareWave t = 1.0 - round(double t) * 2.0
+
+let volume rate value = rate * value
+
+let nl (noteName: NoteName) (octave: int) (sec: float) = sec |> Note(noteName, octave).perCycle |> squareWave |> volume 0.5
 
 [<EntryPoint>]
 let main argv = 
     [0..sampleRate * 2]
-    |> List.map(fun x -> (float32)(Math.Sin(2.0 * Math.PI * pitch * (float)x / (float)sampleRate)))
-    |> FSharpSound.SoundIOOutStreamUtil.playsound sampleRate
+    |> List.map(fun x ->
+        x
+        |> samplesToSec
+        |> (fun x -> (x |> (nl NoteName.c 4)) + (x |> (nl NoteName.e 4)) + (x |> (nl NoteName.g 4)))
+        |> float32)
+    |> SoundIOOutStreamUtil.playsound sampleRate
     0 // return an integer exit code
