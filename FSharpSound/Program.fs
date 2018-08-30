ac61bf1e0d9cb8f7f7bc36af2b850ee5cfee8f6d
aosoft <tan-y@aosoft.jp>
2018-08-30 13:33:48 +0900

    fix names

--- FSharpSound/Program.fs
+++ FSharpSound/Program.fs
@@ -3,22 +3,22 @@ open FSharpSound
 
 let sampleRate = 48000
 
-let samplesToSec sample = (float)sample / (float)sampleRate
+let samplesToSec rate sample = (float)sample / (float)rate
 
 let sinWave t = sin(2.0 * Math.PI * t)
-let squareWave t = 1.0 - round(double t) * 2.0
+let squareWave t = 1.0 - round(float t) * 2.0
 
 let volume rate value = rate * value
 
-let nl (noteName: NoteName) (octave: int) (sec: float) = sec |> Note(noteName, octave).perCycle |> squareWave |> volume 0.5
+let fn noteName octave sec = sec |> Note(noteName, octave).perCycle |> squareWave |> volume 0.3
 
 [<EntryPoint>]
 let main argv = 
     [0..sampleRate * 2]
     |> List.map(fun x ->
         x
-        |> samplesToSec
-        |> (fun x -> (x |> (nl NoteName.c 4)) + (x |> (nl NoteName.e 4)) + (x |> (nl NoteName.g 4)))
+        |> samplesToSec sampleRate
+        |> (fun x -> (x |> (fn NoteName.c 4)) + (x |> (fn NoteName.e 4)) + (x |> (fn NoteName.g 4)))
         |> float32)
     |> SoundIOOutStreamUtil.playsound sampleRate
     0 // return an integer exit code
